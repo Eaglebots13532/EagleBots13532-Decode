9a83fcc135246a9076e2f880baa689639cb97410
Eric Ratliff <e@edr.xyz>
2026-02-12 19:27:30 -0600

    Fixed intendation to 3 spaces

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DecodeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DecodeTeleop.java
@@ -1,27 +1,20 @@
-// Copyright (c) 2025-2026 FTC 13532
+// Copyright (c) 2024-2025 FTC 13532
 // All rights reserved.
 
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.AnalogInput;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import org.firstinspires.ftc.teamcode.chute.Chute;
-import org.firstinspires.ftc.teamcode.chute.ChuteController;
-import org.firstinspires.ftc.teamcode.chute.FtcMotor;
-import org.firstinspires.ftc.teamcode.chute.FtcPotentiometer;
 
 @TeleOp(name = "Decode Teleop")
 public class DecodeTeleop extends LinearOpMode {
 
   @Override
   public void runOpMode() {
-    
+
     waitForStart();
 
     while (opModeIsActive()) {
-
 
       sleep(20); // 50Hz loop
     }

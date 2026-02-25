8ff620fcdd8de3eaa1704baceb6adba8c34d0c78
Eaglebots13532 <89163119+Eaglebots13532@users.noreply.github.com>
2026-02-24 19:17:58 -0600

    Cole's code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cole/Cole.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.cole;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "TankDriveWithTriggers", group = "TeleOp")
+public class Cole extends LinearOpMode {
+
+    private DcMotor leftFront;
+    private DcMotor leftRear;
+    private DcMotor rightFront;
+    private DcMotor rightRear;
+
+
+    public void runOpMode() {
+
+        // Hardware mapping
+        leftFront  = hardwareMap.get(DcMotor.class, "LMotor");
+        leftRear   = hardwareMap.get(DcMotor.class, "BMotor");
+        rightFront = hardwareMap.get(DcMotor.class, "RMotor");
+        rightRear  = hardwareMap.get(DcMotor.class, "FMotor");
+
+        // Reverse left side if needed
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftRear.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // Stick control (Tank Drive)
+            double leftPower  = -gamepad1.left_stick_y;
+            double rightPower = -gamepad1.right_stick_y;
+
+            // Trigger override
+            double forward  = gamepad1.right_trigger;
+            double backward = gamepad1.left_trigger;
+
+            if (forward > 0.05) {
+                leftPower = forward;
+                rightPower = forward;
+            }
+            else if (backward > 0.05) {
+                leftPower = -backward;
+                rightPower = -backward;
+            }
+
+            // Set motor power
+            leftFront.setPower(leftPower);
+            leftRear.setPower(leftPower);
+            rightFront.setPower(rightPower);
+            rightRear.setPower(rightPower);
+
+            telemetry.addData("Left Power", leftPower);
+            telemetry.addData("Right Power", rightPower);
+            telemetry.update();//
+        }
+    }
+}

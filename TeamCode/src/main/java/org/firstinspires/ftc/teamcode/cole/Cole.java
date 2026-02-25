ba9e20e76549cdca6ec1a338e34394ffe44a6943
Eric Ratliff <e@edr.xyz>
2026-02-24 19:50:08 -0600

    gradlew updated tabs and spaces

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cole/Cole.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cole/Cole.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2024-2025 FTC 13532
+// All rights reserved.
+
 package org.firstinspires.ftc.teamcode.cole;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -7,54 +10,52 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "TankDriveWithTriggers", group = "TeleOp")
 public class Cole extends LinearOpMode {
 
-    private DcMotor leftFront;
-    private DcMotor leftRear;
-    private DcMotor rightFront;
-    private DcMotor rightRear;
+  private DcMotor leftFront;
+  private DcMotor leftRear;
+  private DcMotor rightFront;
+  private DcMotor rightRear;
 
+  public void runOpMode() {
 
-    public void runOpMode() {
+    // Hardware mapping
+    leftFront = hardwareMap.get(DcMotor.class, "LMotor");
+    leftRear = hardwareMap.get(DcMotor.class, "BMotor");
+    rightFront = hardwareMap.get(DcMotor.class, "RMotor");
+    rightRear = hardwareMap.get(DcMotor.class, "FMotor");
 
-        // Hardware mapping
-        leftFront  = hardwareMap.get(DcMotor.class, "LMotor");
-        leftRear   = hardwareMap.get(DcMotor.class, "BMotor");
-        rightFront = hardwareMap.get(DcMotor.class, "RMotor");
-        rightRear  = hardwareMap.get(DcMotor.class, "FMotor");
+    // Reverse left side if needed
+    leftFront.setDirection(DcMotor.Direction.REVERSE);
+    leftRear.setDirection(DcMotor.Direction.REVERSE);
 
-        // Reverse left side if needed
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-        leftRear.setDirection(DcMotor.Direction.REVERSE);
+    waitForStart();
 
-        waitForStart();
+    while (opModeIsActive()) {
 
-        while (opModeIsActive()) {
+      // Stick control (Tank Drive)
+      double leftPower = -gamepad1.left_stick_y;
+      double rightPower = -gamepad1.right_stick_y;
 
-            // Stick control (Tank Drive)
-            double leftPower  = -gamepad1.left_stick_y;
-            double rightPower = -gamepad1.right_stick_y;
+      // Trigger override
+      double forward = gamepad1.right_trigger;
+      double backward = gamepad1.left_trigger;
 
-            // Trigger override
-            double forward  = gamepad1.right_trigger;
-            double backward = gamepad1.left_trigger;
+      if (forward > 0.05) {
+        leftPower = forward;
+        rightPower = forward;
+      } else if (backward > 0.05) {
+        leftPower = -backward;
+        rightPower = -backward;
+      }
 
-            if (forward > 0.05) {
-                leftPower = forward;
-                rightPower = forward;
-            }
-            else if (backward > 0.05) {
-                leftPower = -backward;
-                rightPower = -backward;
-            }
+      // Set motor power
+      leftFront.setPower(leftPower);
+      leftRear.setPower(leftPower);
+      rightFront.setPower(rightPower);
+      rightRear.setPower(rightPower);
 
-            // Set motor power
-            leftFront.setPower(leftPower);
-            leftRear.setPower(leftPower);
-            rightFront.setPower(rightPower);
-            rightRear.setPower(rightPower);
-
-            telemetry.addData("Left Power", leftPower);
-            telemetry.addData("Right Power", rightPower);
-            telemetry.update();//
-        }
+      telemetry.addData("Left Power", leftPower);
+      telemetry.addData("Right Power", rightPower);
+      telemetry.update(); //
     }
+  }
 }

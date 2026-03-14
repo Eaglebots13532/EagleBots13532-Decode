e5f055442a1f7b7820f44d385c28809f6f7611a2
EagleNotBot <phibersto26@gmail.com>
2026-03-13 20:14:05 -0500

    works
    on cardinal test. Need to get it to run with servo's. Sends UDP data successfully.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/LearnEncoderStuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/LearnEncoderStuff.java
@@ -54,7 +54,7 @@ public class LearnEncoderStuff extends LinearOpMode {
       motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
       HoodPosition(-2000);
-      HoodPosition(1000);
+      HoodPosition(-1000);
       HoodPosition(-3000);
       // tells what positions to go to
 
@@ -70,13 +70,13 @@ public class LearnEncoderStuff extends LinearOpMode {
 
         // add a check max position eventually
         targetPosition += motor.getCurrentPosition();
-        motor.setTargetPosition(targetPosition);
+        motor.setTargetPosition(newPosition);
         sndData.sendData(eTime.milliseconds(), 2, motor.getCurrentPosition());
-        sndData.sendData(eTime.milliseconds(), 2, targetPosition);
+        sndData.sendData(eTime.milliseconds(), 2, newPosition);
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         // target position!
 
-        motor.setPower(0.2);
+        motor.setPower(0.5);
         eTime.reset();
         while (opModeIsActive() && motor.isBusy()) {
           if (eTime.seconds() > 1) {
